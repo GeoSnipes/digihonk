98bc4d3a7783a748350ec7d623c4763c436bb38f
GeoSnipes <geovanni.fortisyute@gmail.com>
2019-12-11 14:38:57 -0600

    fix time management

--- IoTLogicv2.py
+++ IoTLogicv2.py
@@ -5,7 +5,8 @@ import re
 from argparse import ArgumentParser
 
 MYID = randint(1,100)
-stoptime = 9999999999
+MYSSID = ''
+timer_start = 9999999999
 counter = 0
 my_direction = 8
 
@@ -23,7 +24,17 @@ collision = {
     '11': ['1', '2', '3', '4', '7', '8'],
     '12': ['4', '8']
     }
+def gettime(cnv_time=None):
+    precision = (10**3)
+    
+    if cnv_time != None:
+        return cnv_time/precision
 
+    timenow = time()
+    timenow = int(timenow*(precision))/(10.0**7)
+    timenow = str(timenow).split('.')[1]
+    timenow += '0'*(7-len(timenow))
+    return int(timenow)
 def new_broadcast(mode, misc='', flush=False):
     """Construct broadcast ssid the  arduino is to use
 
@@ -42,22 +53,27 @@ def new_broadcast(mode, misc='', flush=False):
     global MYID
     global counter
     global my_direction 
-    global stoptime
+    global timer_start
+    global MYSSID
     counter += 1
+
     serconn.flush_serial_inout()
+
     if mode != '0':
-        new_ssid = '.'.join([str(MYID), str(counter), str(int(stoptime)),str(mode),str(my_direction),str(misc)])
+        new_ssid = '.'.join([str(MYID), str(counter), str(int(timer_start)),str(mode),str(my_direction),str(misc)])
+        MYSSID = 'DGHonk-'+new_ssid
     else:
         new_ssid = '---'
+
     serconn.update_beacon_ssid(new_ssid)
-    if flush:
+    if not flush:
         serconn.flush_serial_inout()
     else:
         print(new_ssid, end='\t\t\t')
-        print(serconn.read_from_esp(.8))
+        print(serconn.read_from_esp(.4))
 
 
-def get_dghonks(wait=1, entire=True):
+def get_dghonks(wait=.4, entire=True):
     """Retrieve list of all wifi signals
 
     Parameters
@@ -97,7 +113,7 @@ def get_MYID():
     serconn.update_beacon_ssid("2:")
     return serconn.read_from_esp()
 
-def create_inter_dict(inter_SSIDS):
+def create_inter_dict(inter_SSIDS, show_sig=False):
     """Convert list of ssid signals to a dictionary with key id and value stats 
 
     Parameters
@@ -114,15 +130,15 @@ def create_inter_dict(inter_SSIDS):
 
     E.G.
                           num||id.counter.time stopped.mode.direction.misc||channel||signal stregth||mac address
-    input: inter_SSIDS = ['1||DGHonk-1.1.1256489.1.2.||11||-52||18:9C:27:34:42:60', '4||Hennhouse||11||-89||10:93:97:78:EA:40']
+    input: inter_SSIDS = ['1||DGHonk-1.3.1256489.1.2.||11||-52||18:9C:27:34:42:60', '4||Hennhouse||11||-89||10:93:97:78:EA:40']
                             
     output: car_ids = {'1':[    '1',    '1256489',      '1',      '2'       '-52',         '18:9C:27:34:42:60']}
     """
 
     errorhonk = []
     car_ids={}
-    if len(inter_SSIDS) == 0:
-        return {}
+    
+    inter_SSIDS.append(f'0||{MYSSID}||11||-1||00:00:00:00:00:00')
 
     #Check and combine stats to ensure unique car ids
     for wifissid in inter_SSIDS:
@@ -148,6 +164,9 @@ def create_inter_dict(inter_SSIDS):
     
     # if len(errorhonk) > 0:
     #     print(f"Error DGHonk SSID's:\n{errorhonk}")
+    if show_sig:
+        for cid, stat in car_ids.items():
+            print(f'{cid}: {stat}')
     return car_ids
 
 def mode2_scanssid():
@@ -164,9 +183,8 @@ def mode2_scanssid():
 def mode4_immoving():
     """Update ssids with status of moving porgress
     """
-
-    serconn.update_beacon_ssid('dgh:2')
     new_broadcast(mode='4')
+    serconn.update_beacon_ssid('dgh:2')
     '''Read from arduino button press to signify passed intersection'''
     print(f'[INFO] Waiting on notfication passed intersection.')
     while True: 
@@ -185,22 +203,25 @@ def mode5_monitornexttomove(go_after):
     go_after: list
         list of cars to monitor until they are out of intersection
     """
-    
-    new_broadcast(mode='5')
-    for mon in go_after:
-        signal_strength = []
-        for _ in range(2):
-            while True:
-                honks = create_inter_dict(get_dghonks())
-                if mon in honks:
-                    signal_strength.append(int(honks[mon][4]))
-                    if honks[mon][2] == '6':
+    try:
+        new_broadcast(mode='5')
+        for mon in go_after:
+            signal_strength = []
+            for _ in range(2):
+                while True:
+                    honks = create_inter_dict(get_dghonks())
+                    if mon in honks:
+                        signal_strength.append(int(honks[mon][4]))
+                        if honks[mon][2] == '6':
+                            break
+                    else:
                         break
-                else:
-                    break
+    except KeyboardInterrupt:
+        new_broadcast(mode='0')
+        serconn.__exit__
+        quit()
 
-
-def mode7_findallmoving(inter_list):
+def mode7_findallmoving(inter_list, model1=False):
     """Construct iterator of cars that can move and those that cannot
 
     Parameters
@@ -216,7 +237,7 @@ def mode7_findallmoving(inter_list):
 
     global MYID
     global my_direction
-    global stoptime
+    global timer_start
 
     new_broadcast(mode='7')
 
@@ -226,25 +247,27 @@ def mode7_findallmoving(inter_list):
     shouldwait = []
 
     # Add myself to turn intention list
-    turn_intents= {str(my_direction): (str(MYID), stoptime)}
-    
+    # turn_intents= {str(my_direction): (str(MYID), timer_start)}
+    turn_intents = {}
     # Add remaining cars to turn intention
     for car in inter_list:
         if inter_list[car][2] == '6':
             continue
         intent = inter_list[car][3]
         turn_intents[intent] = (car, int(inter_list[car][1]))
-    print(turn_intents)
-    print(inter_list)
+    # print(turn_intents)
     sorted_intents = sorted(turn_intents.items(), key=lambda x: int(x[1][1]))
     
     # Add each car to allowed to move and create a set of directions that will collide
     for intent, id_time in sorted_intents:
         all_cars.add(id_time[0])
+    for intent, id_time in sorted_intents:
         if (intent not in will_collide):
             moving.append(id_time[0])
             will_collide.update(collision[intent])
-    
+        if model1:
+            break
+
     # Collect all car is not able to turn
     shouldwait = all_cars.difference(set(moving))
     return moving, shouldwait
@@ -267,49 +290,46 @@ honk_list = [\
     '5||Heathers wi fi||11||-90||3C:7A:8A:96:D7:D2']
 
 if __name__ == '__main__':
-    ESPSerial.commPort = 'COM23'
+    dgh_timers = []
 
     parser = ArgumentParser()
-    parser.add_argument('-p', dest='port', action='store', help='Supply port (WIN only)', default=ESPSerial.commPort)
+    parser.add_argument('-p', dest='port', action='store', help='Supply port (WIN only)', default= 'COM23')
     parser.add_argument('-d', dest='dir', action='store', help='Supply direction intent', default=8)
+    parser.add_argument('-i', dest='id', action='store', help='Set id to use', default=MYID)
     results = parser.parse_args()
 
+    ESPSerial.commPort  = results.port
     my_direction = int(results.dir)
-    # MYID = get_MYID().strip()
+    MYID = results.id
     with ESPSerial.ESPConnect(results.port) as serconn:
         serconn.update_beacon_ssid('dgh:0')
         while True:
             print(f'[INFO] DGH startup. MYID: {MYID}\t\tDirection:{my_direction}')
-            sleep(1)
             new_broadcast(mode='0')
             print(f'[INFO] Waiting on notification at intersection.')
             while True: 
                 start_dgh = serconn.read_from_esp()
                 if start_dgh == 'dgh:1':
                     break
-            stoptime = time()
+            timer_start = gettime()
             while True:
                 print(f'[INFO] Scanning for DGHonks.')
                 honk_list = mode2_scanssid()
                 print(f'[INFO] Finding collisions.')
-                inter_list = create_inter_dict(honk_list)
-                if len(inter_list) > 0:
-                    mov=[]
-                    col=set()
-                    print(f'[INFO] Found {len(inter_list)} DGH devices.')
+                inter_list = create_inter_dict(honk_list, show_sig=False)
+                if len(inter_list) > 1:
                     mov, col = mode7_findallmoving(inter_list)
                     print(f'[INFO] Allowed to move: {mov}\nMust wait: {col}')
                     if str(MYID) in  mov:
                         break
                     else:
-                        print(f'[INFO] Collision found. Waiting on {mov}')
                         mode5_monitornexttomove(mov)
                         continue
                 break
             print(f'[INFO] Permission to go received. {MYID} will go.')
             mode4_immoving()
             print(f'[INFO] Passed intersection notification received.')
-            sleep(4)
             new_broadcast(mode='0')
+            sleep(2)
             break
         
